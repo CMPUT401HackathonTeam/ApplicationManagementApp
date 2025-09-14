a9f816320dde936ac372abf10d02016aa5c53741
Seerat Samra <seeratsamra@Seerats-MacBook-Pro.local>
2025-09-13 18:09:31 -0600

    us 5.1 and 5.2 done

--- myApplicationManager/urls.py
+++ myApplicationManager/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import MyLoginView, register,homepage
+from .views import MyLoginView, register, homepage, view_applications, update_application_status, get_applications_data
 
 app_name= 'myApplicationManager'
 urlpatterns = [
@@ -10,4 +10,8 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('home/', homepage, name='homepage'),
     
+    # US 5.1 & 5.2: Applications functionality
+    path('applications/', view_applications, name='view_applications'),
+    path('api/applications/', get_applications_data, name='get_applications_data'),
+    path('api/applications/update-status/', update_application_status, name='update_application_status'),
 ]
